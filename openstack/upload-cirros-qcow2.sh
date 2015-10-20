a30f72ecb4fb425ff4bff7e82f77a8273ea567f2
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2015-10-20 20:13:27 +0300

    Fixed typo

--- openstack/upload-cirros-qcow2.sh
+++ openstack/upload-cirros-qcow2.sh
@@ -1,1 +1,1 @@
-glance image-create --name=cirros --container-format=bare --copy-from='http://download.cirros-cloud.net/0.3.3/cirros-0.3.3-x86_64-disk.img' --is-public disk-format=qcow2 --progress
+glance image-create --name=cirros --container-format=bare --copy-from='http://download.cirros-cloud.net/0.3.3/cirros-0.3.3-x86_64-disk.img' --is-public=True --disk-format=qcow2 --progress
