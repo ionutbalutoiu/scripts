9386377d8719d38b443eb54d6ff3c3f1037eef9a
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2015-10-24 15:11:25 +0300

    Organized OpenStack scripts

--- openstack/basic-test/vol-switch.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-VOL_ID=`cinder volume-list | grep test_vol | awk '{print $2}'`
-
-nova volume-detach test $VOL_ID
-while [ -z "`cinder volume-list | grep test_vol | grep available`" ]; do
-    sleep 1
-done
-nova volume-attach test_2 $VOL_ID
