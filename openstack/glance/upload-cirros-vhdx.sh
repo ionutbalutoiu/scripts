c015d78956babd3e0e37de4f6fb6b26b7c779cc6
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2016-04-04 21:28:13 +0300

    upload img twice

--- openstack/glance/upload-cirros-vhdx.sh
+++ openstack/glance/upload-cirros-vhdx.sh
@@ -3,4 +3,5 @@
 sudo apt-get install git -y
 git clone https://github.com/cloudbase/ci-overcloud-init-scripts.git /tmp/ci-overcloud-init-scripts
 glance image-create --visibility public --property hypervisor_type=hyperv --name cirros-vhdx --disk-format vhd --container-format bare --file /tmp/ci-overcloud-init-scripts/scripts/devstack_vm/cirros.vhdx
+glance image-create --visibility public --property hypervisor_type=hyperv --name cirros --disk-format vhd --container-format bare --file /tmp/ci-overcloud-init-scripts/scripts/devstack_vm/cirros.vhdx
 rm -rf /tmp/ci-overcloud-init-scripts
