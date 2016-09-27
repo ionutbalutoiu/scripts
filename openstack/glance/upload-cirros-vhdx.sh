06c9b548303fb4aec7c269e4661a8c3cd84fb40a
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2016-09-27 16:43:04 +0300

    Use openstack client to upload both gen1 & gen2 cirros

--- openstack/glance/upload-cirros-vhdx.sh
+++ openstack/glance/upload-cirros-vhdx.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
-wget http://balutoiu.com/ionut/cirros-gen2.vhdx -O /tmp/cirros.vhdx
-glance image-create --visibility public --property hw_machine_type=hyperv-gen2 --property hypervisor_version_requires='>=6.3' --property hypervisor_type=hyperv --name cirros --disk-format vhd --container-format bare --file /tmp/cirros.vhdx
-rm /tmp/cirros.vhdx --progress
+wget http://balutoiu.com/ionut/cirros-gen2.vhdx -O /tmp/cirros-gen2.vhdx
+openstack image create --public \
+                       --property hw_machine_type=hyperv-gen2 \
+                       --property hypervisor_version_requires='>=6.3' \
+                       --property hypervisor_type=hyperv \
+                       --disk-format vhd \
+                       --container-format bare \
+                       --file /tmp/cirros-gen2.vhdx \
+                       cirros-gen2
+rm /tmp/cirros-gen2.vhdx
+
+wget https://github.com/cloudbase/ci-overcloud-init-scripts/raw/master/scripts/devstack_vm/cirros.vhdx -O /tmp/cirros-gen1.vhdx
+openstack image create --public \
+                       --property hypervisor_type=hyperv \
+                       --disk-format vhd \
+                       --container-format bare \
+                       --file /tmp/cirros-gen1.vhdx \
+                       cirros-gen1
+rm /tmp/cirros-gen1.vhdx
--- openstack/set-up-controller.sh
+++ openstack/set-up-controller.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+DIR=$(dirname $0)
 $DIR/glance/upload-cirros-vhdx.sh
 $DIR/nova/open-all-ports.sh default
 $DIR/neutron/create-topology.sh 192.168.101.0/24 192.168.101.3 192.168.101.222 192.168.101.230 10.0.1.0/24 192.168.101.3 vlan
