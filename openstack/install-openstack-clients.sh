221ab107972f7619c3322554f1657462e9b11132
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2021-01-08 18:23:19 +0000

    Generalize openstack/install-openstack-clients.sh

--- openstack/install-openstack-clients.sh
+++ openstack/install-openstack-clients.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
+set -e
 
-if [[ "$1" != "pike" ]] && [[ "$1" != "newton" ]] && [[ "$1" != "ocata" ]]; then
-    echo "USAGE: $0 <newton/ocata/pike>"
+if [[ $# -ne 1 ]]; then
+    echo "USAGE: $0 <release_name>"
     exit 1
 fi
 
-APPS="gcc python-dev"
-echo "Need sudo password to install: $APPS"
-sudo apt install $APPS -y
-
 BRANCH="stable/$1"
 
-pip install git+https://github.com/openstack/python-openstackclient.git@$BRANCH
-pip install git+https://github.com/openstack/python-neutronclient.git@$BRANCH
-pip install git+https://github.com/openstack/python-novaclient.git@$BRANCH
-pip install git+https://github.com/openstack/python-glanceclient.git@$BRANCH
-pip install git+https://github.com/openstack/python-cinderclient.git@$BRANCH
-pip install git+https://github.com/openstack/python-heatclient.git@$BRANCH
-pip install git+https://github.com/openstack/python-magnumclient.git@$BRANCH
+pip3 install git+https://github.com/openstack/python-openstackclient.git@$BRANCH
+pip3 install git+https://github.com/openstack/python-neutronclient.git@$BRANCH
+pip3 install git+https://github.com/openstack/python-novaclient.git@$BRANCH
+pip3 install git+https://github.com/openstack/python-glanceclient.git@$BRANCH
+pip3 install git+https://github.com/openstack/python-cinderclient.git@$BRANCH
+pip3 install git+https://github.com/openstack/python-heatclient.git@$BRANCH
+pip3 install git+https://github.com/openstack/python-magnumclient.git@$BRANCH
